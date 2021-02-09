90db0a77697b20a9305cd3e26fcabee221d00bbe
Slobolinskyi <vitalii.slobolinskyi@accenture.com>
2021-02-09 11:45:48 +0200

    fixes2

---
+++ src/components/molecules/Building.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<BuildingImage v-bind:image="image" v-bind:key="image.id" v-for="image in images"/>
+	</div>	
+</template>
+
+<script>
+	import BuildingImage from "../atoms/BuildingImage.vue"
+	import json from "../../../data/structure.json"
+	export default {
+		name: "img2",
+		components: {
+			BuildingImage
+		},
+		data() {
+			return {
+				images: json.building.images
+			}
+		}
+	}
+</script>
