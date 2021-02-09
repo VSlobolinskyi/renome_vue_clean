069ceba5ac9acb0289fc0894a259d811268e4651
VSlobolinskyi <32908303+VSlobolinskyi@users.noreply.github.com>
2021-02-09 11:30:57 +0200

    Revert "Revert "V1.1""

--- src/components/atoms/aboutText.vue
+++ src/components/atoms/aboutText.vue
@@ -1,16 +1,14 @@
 <template>
-	<p class="about__text">{{text.innerText}}</p>
+	<p class="about__text">{{about.text}}</p>
 </template>
 
 <script>
+	import json from "../../../data/structure.json"
 	export default {
 		name: "aboutText",
 		data() {
 			return {
-				text: {
-					id: 1,
-					innerText: "Renome it is renowned restaurant  delights the senses with elegant ambiance, gracious service and delectable menus in the heart Of Manhattan's. The distinctive setting boasts contemporary furnishings and finishes atop the restaurant's grand modern architecture. Renome offers the room for private dining where guests enjoy creative foods and sumptuous late night desserts. Is one Of the few Manhattan restaurants to have a coveted 3—Star Michelin rating."
-				}
+				about: json.about
 			}
 		}
 	}
---
+++ src/components/molecules/NavigationIcons.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="icons">
+		<cart/>
+		<cartStatus/>
+		<stick/>
+		<hamburgerButton/>
+		<corss/>
+	</div>	
+</template>
+
+<script>
+	import cart from "../atoms/Cart.vue"
+	import stick from "../atoms/Stick.vue"
+	import corss from "../atoms/Cross.vue"
+	import hamburgerButton from "../atoms/HamburgerButton.vue"
+	import cartStatus from "../atoms/CartStatus.vue"
+	export default {
+		name: "navigationIcons",
+		components: {
+			cart,
+			cartStatus,
+			stick,
+			hamburgerButton,
+			corss
+		}
+	}
+</script>
+
+<style scoped>
+	.icons {
+		position: relative;
+		display: flex;
+		align-items: center;
+		padding: 5px 10px 0 0;
+	}
+</style>
